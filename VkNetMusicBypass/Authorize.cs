2b94e3b68be52495cca16b9ba717c2b7ee1ccf44
atckun <stasc43@gmail.com>
2018-07-13 04:53:25 +0300

    Create project

---
+++ VkNetMusicBypass/Authorize.cs
@@ -0,0 +1,97 @@
+﻿using VkNet;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using VkNet.Model;
+using VkNet.Utils;
+using System;
+
+namespace VkNetMusicBypass
+{
+    class Authorize
+    {
+        public VkApi api;
+
+        public Authorize(VkApi _api)
+        {
+            api = _api;
+        }
+
+        public void Auth(string token)
+        {
+            api.Authorize(new ApiAuthParams()
+            {
+                AccessToken = token
+            });
+        }
+
+        /// <summary>
+        /// Получение токена через логин и пароль
+        /// </summary>
+        /// <param name="login">Логин</param>
+        /// <param name="password">Пароль</param>
+        /// <returns></returns>
+        public void Auth(string login, string password)
+        {
+            string URL = $"https://oauth.vk.com/token?grant_type=password&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username={login}&password={password}";
+            var wc = new WebClient();
+            string response = wc.DownloadString(URL);
+            var json = JObject.Parse(response);
+
+            wc.Dispose();
+            Auth(json["access_token"].ToString());
+            Auth(Bypass()); 
+        }
+
+        public string Bypass()
+        {
+            MessagesPushSettings messagesPush = new MessagesPushSettings()
+            {
+                NoSound = false,
+                NoText = false
+            };
+
+            api.Account.RegisterDevice(new VkNet.Model.RequestParams.AccountRegisterDeviceParams()
+            {
+                Token = "LZA-UH0bUzN:APA91bHuxO87e5Mb975w4zBXuC-oNs61_SiRKg6c1oHHMefNTVbUacI5WKQ_Lfb_HlF-ZhGsXqvfzxcJk5Wcd03oGCFTkepUXt_lLrtcnKBzniXdkMFmuLpuvxR5lyts1D6lp9YnSXcDnv_bPjbJH0WWS7_7pPsHqg",
+                DeviceId = "a3478da7eb6f0804",
+                DeviceModel = "Razer",
+                SystemVersion = "7.1.1",
+                Settings = new PushSettings()
+                {
+                    FriendFound = true,
+                    Birthday = true,
+                    SdkOpen = true,
+                    GroupAccepted = true,
+                    WallPublish = true,
+                    AppRequest = true,
+                    WallPost = true,
+                    GroupInvite = true,
+                    FriendAccepted = true,
+                    TagPhoto = true,
+                    Comment = true,
+                    EventSoon = true,
+                    NewPost = true,
+                    Reply = true,
+                    Repost = true,
+                    Mention = true,
+                    Friend = true,
+                    Like = true,
+                    Chat = messagesPush,
+                    Msg = messagesPush
+                }
+            });
+
+
+            string[] receipt = new string[] { "zEXKaMARS_h:APA91bHXsKM-p7TA1cQ_dkKsDD3dk6pTlvS7egJvkfzNXpNsYIpyYZeHIiMfCgHeEKOaQ-RRJcma7vrDZF7gCWnqtkXA9TRjewqg8iXOJCdxoVuPHi-23qkGjDg0ygGfl6k_APr1Vb9COTPi9KDvMVDG4a9MUZ_waQ",
+                    "7AJXiv9OMFx:APA91bF6eR6NGn3SSIkL152A610JzdjC616tBb0TBoyENXOVAQDeLb1JdZEn4RTvkV3Hoxn0VATuO4U1fTHOJCfpieAvFDVM_GjFgkkLAZev2bPdyA9rqDkjUK5AzboRP-nXoJ6t1FMRkyNIWYNnB66xFrLPInq1KA",
+                    "zIA9dkystyi:APA91bGLOkfX_Maad2q_MgLV6BmSHXdeQ99K5GbGnvVZ7BLLYXdapQahJXCb1AEOafFAoMf6GY-ql0UedVK6EZd6dIUcIn8ALgqW-KMNzs1yKGOe-Jakxe8Tfr7bCkFbY6UDA5RCph4KqqIilZDEEFZ8w8PPzWAAQA" };
+
+            var response = api.Call("auth.refreshToken", new VkParameters()
+                {
+                    { "receipt", receipt[new Random().Next(receipt.Length)] }
+                });
+
+            return JObject.Parse(response.RawJson)["response"]["token"].ToString();
+        }
+    }
+}
---
+++ VkNetMusicBypass/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using VkNet;
+using System.Collections.ObjectModel;
+using VkNet.Model;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet.Abstractions.Utils;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace VkNetMusicBypass
+{
+    class Program
+    {
+        static VkApi api;
+        static Authorize auth;
+
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.TryAddSingleton<IRestClient, RestClient>();
+            api = new VkApi(serviceCollection);
+            auth = new Authorize(api);
+
+            string token = File.ReadAllText("token.txt");
+
+            if (!string.IsNullOrEmpty(token))
+                auth.Auth(token);
+                
+            else
+                auth.Auth("login", "password");
+
+            //Один токен после манипуляций с обходом может работать дальше
+            File.WriteAllText("token.txt", api.Token);
+
+            var audios = Get();
+            foreach (var track in audios)
+            {
+                Console.WriteLine($"{track.Artist} - {track.Title}");
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Пример использования
+        /// </summary>
+        /// <returns></returns>
+        private static ReadOnlyCollection<VkNet.Model.Attachments.Audio> Get()
+        {
+            User user = null;
+
+            try
+            {
+                return api.Audio.Get(out user, new VkNet.Model.RequestParams.AudioGetParams()
+                {
+                    Count = 40,
+                    OwnerId = 62103461
+                });
+            }
+            catch (VkNet.Exception.VkApiException)
+            {
+                //token confirmation required exception
+                string newToken = auth.Bypass();
+                auth.Auth(newToken);
+                File.WriteAllText("token.txt", newToken); //Сохранить токен после манипуляция с обходом
+                return Get();
+            }
+        }
+    }
+}
