f2a8a9225a7ac29734773abcf6b650abe3ee8dd3
itsokto <picnet9@gmail.com>
2018-08-01 23:38:46 +0300

    Проект переработан.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Abstractions.Utils;
+using VkNet.AudioBypassService;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static IVkApi _api;
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IBrowser, Authorization>();
+            serviceCollection.AddSingleton<IRestClient, RestClientEx>();
+            serviceCollection.AddSingleton<IReceiptParser, ReceiptParser>();
+            _api = new VkApi(serviceCollection);
+
+            _api.Authorize(new ApiAuthParams
+            {
+                Login = "89655341526",
+                Password = "AludaG13"
+            });
+
+            var audios = _api.Audio.Get(out var user, new AudioGetParams { Count = 10 });
+            foreach (var audio in audios)
+            {
+                Console.WriteLine($" > {audio.Artist} - {audio.Title}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- ConsoleApp/Properties/AssemblyInfo.cs
+++ ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("VkNetMusicBypass")]
+[assembly: AssemblyTitle("ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VkNetMusicBypass")]
+[assembly: AssemblyProduct("ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("3feb88cb-cece-4cfb-99f1-bf4d5520a06a")]
+[assembly: Guid("38a98df2-e337-4ddc-918b-ad9b6207817d")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
