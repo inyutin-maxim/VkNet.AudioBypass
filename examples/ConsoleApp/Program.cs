48e103cf041ad7804952535ba29e4b4d627b7a5d
itsokto <picnet9@gmail.com>
2018-11-25 17:18:17 +0300

    Обновил зависимости ConsoleApp

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -2,37 +2,36 @@
 using Microsoft.Extensions.DependencyInjection;
 using VkNet;
 using VkNet.Abstractions;
-using VkNet.Abstractions.Utils;
-using VkNet.AudioBypassService;
+using VkNet.AudioBypassService.Extensions;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
-using VkNet.Utils;
 
 namespace ConsoleApp
 {
-    class Program
+    internal class Program
     {
         private static IVkApi _api;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton<IBrowser, Authorization>();
-            serviceCollection.AddSingleton<IRestClient, RestClientEx>();
-            serviceCollection.AddSingleton<IReceiptParser, ReceiptParser>();
+            serviceCollection.AddAudioBypass();
+
             _api = new VkApi(serviceCollection);
 
             _api.Authorize(new ApiAuthParams
             {
                 Login = "ЛОГИН",
-                Password = "ПАРОЛЬ"
+                Password = "ПАРОЛЬ",
+                TwoFactorAuthorization = () =>
+                {
+                    Console.WriteLine(" > Введите код:");
+                    return Console.ReadLine();
+                }
             });
 
             var audios = _api.Audio.Get(new AudioGetParams {Count = 10});
-            foreach (var audio in audios)
-            {
-                Console.WriteLine($" > {audio.Artist} - {audio.Title}");
-            }
+            foreach (var audio in audios) Console.WriteLine($" > {audio.Artist} - {audio.Title}");
 
             Console.ReadLine();
         }
