adcfab2ce0b249ffa7e6b70a3094cd89edeb73d5
Dzianis Shastak <shastak.dzianis@gmail.com>
2023-07-24 21:35:12 +0300

    fix compatibility with VkNet 1.75.0

--- VkNet.AudioBypassService/Utils/RestClientWithUserAgent.cs
+++ VkNet.AudioBypassService/Utils/RestClientWithUserAgent.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using VkNet.Abstractions.Utils;
@@ -9,7 +13,7 @@ namespace VkNet.AudioBypassService.Utils
 {
 	/// <inheritdoc cref="IRestClient" />
 	[UsedImplicitly]
-	public class RestClientWithUserAgent : RestClient
+	public class RestClientWithUserAgent : IRestClient
 	{
 		private static readonly IDictionary<string, string> VkHeaders = new Dictionary<string, string>
 		{
@@ -17,12 +21,34 @@ namespace VkNet.AudioBypassService.Utils
 			{ "x-vk-android-client", "new" }
 		};
 
-		public RestClientWithUserAgent(HttpClient httpClient, ILogger<RestClient> logger) : base(httpClient, logger)
+		private readonly RestClient _restClientProxy;
+
+		public RestClientWithUserAgent(HttpClient httpClient, ILogger<RestClient> logger)
 		{
+			_restClientProxy = new RestClient(httpClient, logger);
+
 			foreach (var header in VkHeaders)
 			{
-				httpClient.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
+				_restClientProxy.HttpClient.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
 			}
+		}
+
+		/// <inheritdoc />
+		public Task<HttpResponse<string>> GetAsync(Uri uri, IEnumerable<KeyValuePair<string, string>> parameters, Encoding encoding, CancellationToken token = default)
+		{
+			return _restClientProxy.GetAsync(uri, parameters, encoding, token);
+		}
+
+		/// <inheritdoc />
+		public Task<HttpResponse<string>> PostAsync(Uri uri, IEnumerable<KeyValuePair<string, string>> parameters, Encoding encoding, IEnumerable<KeyValuePair<string, string>> headers = null, CancellationToken token = default)
+		{
+			return _restClientProxy.PostAsync(uri, parameters, encoding, headers, token);
+		}
+
+		/// <inheritdoc />
+		public void Dispose()
+		{
+			_restClientProxy?.Dispose();
 		}
 	}
 }
